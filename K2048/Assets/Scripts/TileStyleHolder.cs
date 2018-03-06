e31606b7b1050b96880a1932d9f86006ee45ed80
TangqiFeng <kylezete@gmail.com>
2018-03-06 17:53:33 +0000

    storing color info for different tiles

---
+++ K2048/Assets/Scripts/TileStyleHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileStyle {
+	
+	public int Number;
+	public Color32 TileColor;
+	public Color32 TextColor;
+}
+
+public class TileStyleHolder : MonoBehaviour {
+
+	// singleton 
+	private static TileStyleHolder Instance;
+
+	public TileStyle[] TileStyles;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+}
