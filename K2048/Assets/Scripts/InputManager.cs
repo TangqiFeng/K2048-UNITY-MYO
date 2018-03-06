5464a9c5ce128cdc0abc21dd95e354c74a116a35
TangqiFeng <kylezete@gmail.com>
2018-03-06 15:42:22 +0000

    Reading input from keyboard

---
+++ K2048/Assets/Scripts/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection
+{
+	Left, Right, Up, Down
+}
+
+public class InputManager : MonoBehaviour {
+
+	private GameManager gm;
+
+	void Awake ()
+	{
+		gm = GameObject.FindObjectOfType<GameManager> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			// left move
+			gm.Move(MoveDirection.Left); 
+		}else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			// right move
+			gm.Move(MoveDirection.Right); 
+		}else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			// up move
+			gm.Move(MoveDirection.Up); 
+		}else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			// down move
+			gm.Move(MoveDirection.Down); 
+		}
+	}
+}
