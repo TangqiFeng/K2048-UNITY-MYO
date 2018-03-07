567b9bb9362cb34b9273bef8b0acc2c290bdd9de
TangqiFeng <kylezete@gmail.com>
2018-03-07 12:19:38 +0000

    randomly generate new tile (2/4)

--- K2048/Assets/Scripts/GameManager.cs
+++ K2048/Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour {
 
 	// store tiles (in Start method, the array will be deleted after executed)
 	private Tile[,] AllTiles = new Tile[4, 4];
+	// use list to store empty tiles
+	private List<Tile> EmptyTiles = new List<Tile>();
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +19,32 @@ public class GameManager : MonoBehaviour {
 			t.Number = 0;
 			// add tile to alltiles array
 			AllTiles[t.indRow, t.indCol] = t;
+			// add to empty tile list
+			EmptyTiles.Add(t);
+		}
+	}
+
+	// generate a new tile (with number 2/4)
+	void Generate(){
+		// if empty tile exists, then generate a tile (2/4) in random position.
+		if (EmptyTiles.Count > 0) {
+			int indexForNewNumber = Random.Range (0, EmptyTiles.Count);
+			// create a random number to control generate 4
+			int RandomNumber = Random.Range(0,10);
+			if(RandomNumber == 0)
+				EmptyTiles [indexForNewNumber].Number = 4;
+			else
+				EmptyTiles [indexForNewNumber].Number = 2;
+			EmptyTiles.RemoveAt (indexForNewNumber);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// add event to test method 'Generate'
+		if (Input.GetKeyDown (KeyCode.G)) {
+			Generate ();
+		}
 	}
 
 	public void Move (MoveDirection md)
