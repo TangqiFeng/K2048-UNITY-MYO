d1598de511f30cb0ec16b7b2f1500f6755de3be3
TangqiFeng <kylezete@gmail.com>
2018-03-07 20:56:54 +0000

    create lists of rows and columns

--- K2048/Assets/Scripts/GameManager.cs
+++ K2048/Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour {
 
 	// store tiles (in Start method, the array will be deleted after executed)
 	private Tile[,] AllTiles = new Tile[4, 4];
+	// add tile array lists to store rows and columns
+	private List<Tile[]> rows = new List<Tile[]> ();
+	private List<Tile[]> columns = new List<Tile[]> ();
 	// use list to store empty tiles
 	private List<Tile> EmptyTiles = new List<Tile>();
 
@@ -22,6 +25,17 @@ public class GameManager : MonoBehaviour {
 			// add to empty tile list
 			EmptyTiles.Add(t);
 		}
+
+		// add rows and columns to tile array list
+		rows.Add(new Tile[]{AllTiles[0,0],AllTiles[0,1],AllTiles[0,2],AllTiles[0,3]});
+		rows.Add(new Tile[]{AllTiles[1,0],AllTiles[1,1],AllTiles[1,2],AllTiles[1,3]});
+		rows.Add(new Tile[]{AllTiles[2,0],AllTiles[2,1],AllTiles[2,2],AllTiles[2,3]});
+		rows.Add(new Tile[]{AllTiles[3,0],AllTiles[3,1],AllTiles[3,2],AllTiles[3,3]});
+		columns.Add(new Tile[]{AllTiles[0,0],AllTiles[1,0],AllTiles[2,0],AllTiles[3,0]});
+		columns.Add(new Tile[]{AllTiles[0,1],AllTiles[1,1],AllTiles[2,1],AllTiles[3,1]});
+		columns.Add(new Tile[]{AllTiles[0,2],AllTiles[1,2],AllTiles[2,2],AllTiles[3,2]});
+		columns.Add(new Tile[]{AllTiles[0,3],AllTiles[1,3],AllTiles[2,3],AllTiles[3,3]});
+
 	}
 
 	// generate a new tile (with number 2/4)
